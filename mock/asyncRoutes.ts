6d6a43c3804a65027f8efc56288fdb4cdd2235c6
zhanggq52434 <zhanggq52434@hundsun.com>
2024-11-12 12:26:26 +0800

    feat: 删除 tabs 页面和 about 页

--- mock/asyncRoutes.ts
+++ mock/asyncRoutes.ts
@@ -1,6 +1,6 @@
 // 模拟后端动态生成路由
 import { defineFakeRoute } from "vite-plugin-fake-server/client";
-import { system, monitor, permission, frame, tabs } from "@/router/enums";
+import { system, monitor, permission, frame } from "@/router/enums";
 
 /**
  * roles：页面级别权限，这里模拟二种 "admin"、"common"
@@ -278,48 +278,6 @@ const frameRouter = {
   ]
 };
 
-const tabsRouter = {
-  path: "/tabs",
-  meta: {
-    icon: "ri:bookmark-2-line",
-    title: "menus.pureTabs",
-    rank: tabs
-  },
-  children: [
-    {
-      path: "/tabs/index",
-      name: "Tabs",
-      meta: {
-        title: "menus.pureTabs",
-        roles: ["admin", "common"]
-      }
-    },
-    // query 传参模式
-    {
-      path: "/tabs/query-detail",
-      name: "TabQueryDetail",
-      meta: {
-        // 不在menu菜单中显示
-        showLink: false,
-        activePath: "/tabs/index",
-        roles: ["admin", "common"]
-      }
-    },
-    // params 传参模式
-    {
-      path: "/tabs/params-detail/:id",
-      component: "params-detail",
-      name: "TabParamsDetail",
-      meta: {
-        // 不在menu菜单中显示
-        showLink: false,
-        activePath: "/tabs/index",
-        roles: ["admin", "common"]
-      }
-    }
-  ]
-};
-
 export default defineFakeRoute([
   {
     url: "/get-async-routes",
@@ -331,8 +289,7 @@ export default defineFakeRoute([
           systemManagementRouter,
           systemMonitorRouter,
           permissionRouter,
-          frameRouter,
-          tabsRouter
+          frameRouter
         ]
       };
     }
